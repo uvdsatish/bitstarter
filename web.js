6b12b128aa8d89b9f37113b47878db04f78b23c3
Satish Udayagiri <uvdsatish@gmail.com>
2013-07-20 22:47:28 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+
+buf=fs.readFileSync("index.html","utf-8");
+
 app.get('/', function(request, response) {
-  response.send('I Love Bindu Kadiam!');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
